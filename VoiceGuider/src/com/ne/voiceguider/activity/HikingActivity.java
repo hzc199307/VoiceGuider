e847622b9e7eddbd1e1cf351aeab9b0d9519db73
xu shenglin <xsl19901125@126.com>
2014-05-15 17:28:39 +0800

    界面

--- VoiceGuider/src/com/ne/voiceguider/activity/HikingActivity.java
+++ VoiceGuider/src/com/ne/voiceguider/activity/HikingActivity.java
@@ -1,69 +1,19 @@
 package com.ne.voiceguider.activity;
 
-import com.ne.voiceguider.R;
-import com.ne.voiceguider.R.id;
-import com.ne.voiceguider.R.layout;
-import com.ne.voiceguider.R.menu;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class HikingActivity extends ActionBarActivity {
+import com.ne.voiceguider.R;
+
+public class HikingActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hiking);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hiking, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_hiking,
-					container, false);
-			return rootView;
-		}
-	}
-
+	
 }
