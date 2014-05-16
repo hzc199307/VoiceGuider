8e2ca9950970a3ecad34fc266930437e0b4dc7d7
He Zhichao <hzc199307@qq.com>
2014-05-16 18:52:04 +0800

    地图部分初步

--- VoiceGuider/src/com/ne/voiceguider/activity/CityActivity.java
+++ VoiceGuider/src/com/ne/voiceguider/activity/CityActivity.java
@@ -1,5 +1,9 @@
 package com.ne.voiceguider.activity;
 
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.MapController;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.platform.comapi.basestruct.GeoPoint;
 import com.ne.voiceguider.R;
 import com.ne.voiceguider.R.id;
 import com.ne.voiceguider.R.layout;
@@ -8,62 +12,51 @@ import com.ne.voiceguider.R.menu;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.view.Window;
 import android.os.Build;
 
-/**
- * 
- * @ClassName: HikingActivity 
- * @Description: TODO 
- * @author HeZhichao
- * @date 2014年5月16日 上午11:18:57 
- *
- */
-public class HikingActivity extends ActionBarActivity {
+public class CityActivity extends ActionBarActivity {
 
-
-	private RelativeLayout hiking_city ;
+	private BMapManager mBMapMan = null;
+	private MapView mMapView = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hiking);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		initMap();
 
-		hiking_city = (RelativeLayout)findViewById(R.id.hiking_city);
-		hiking_city.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				Intent intent = new Intent(HikingActivity.this,CityActivity.class); // 跳转到城市景点详情页面
-				startActivity(intent);
-			}
-		});
 	}
-	class HikingCityOnClickListener implements RelativeLayout.OnClickListener
+	
+	
+	void initMap()
 	{
-
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			Intent intent = new Intent(HikingActivity.this,
-					CityActivity.class); // 跳转到主页面
-			startActivity(intent);
-		}
-
+		mBMapMan=new BMapManager(getApplication());
+		mBMapMan.init(null); 
+		//注意：请在试用setContentView前初始化BMapManager对象，否则会报错
+		setContentView(R.layout.activity_city);
+		mMapView=(MapView)findViewById(R.id.bmapsView);
+		mMapView.setBuiltInZoomControls(true);
+		//设置启用内置的缩放控件
+		MapController mMapController=mMapView.getController();
+		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放
+		GeoPoint point =new GeoPoint((int)(39.915* 1E6),(int)(116.404* 1E6));
+		//用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
+		mMapController.setCenter(point);//设置地图中心点
+		mMapController.setZoom(12);//设置地图zoom级别
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hiking, menu);
+		getMenuInflater().inflate(R.menu.city, menu);
 		return true;
 	}
 
@@ -78,7 +71,6 @@ public class HikingActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
 
 
 
